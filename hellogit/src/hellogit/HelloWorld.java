0d40ec32a3357d84f1824b97864c79c4a3eacebe
17cc0151 <17cc0151@HSS-101-11.westwindsor.k12.nj.us>
2015-10-08 14:22:15 -0400

    dsasdasda

--- hellogit/src/hellogit/HelloWorld.java
+++ hellogit/src/hellogit/HelloWorld.java
@@ -2,6 +2,6 @@ package hellogit;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World and Git!");
+		System.out.println("Hello World and Git! this is a test yo");
 	}
 }
